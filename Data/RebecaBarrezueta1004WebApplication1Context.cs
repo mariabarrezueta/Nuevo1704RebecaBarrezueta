22c3b2ee6745c96f3a21d7c5c2e622975ca6fee2
María Rebeca Barrezueta Arrieta <maria.barrezueta@udla.edu.ec>
2024-04-10 11:36:07 -0500

    Add project files.

---
+++ Data/RebecaBarrezueta1004WebApplication1Context.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using RebecaBarrezueta1004WebApplication1.Models;
+
+namespace RebecaBarrezueta1004WebApplication1.Data
+{
+    public class RebecaBarrezueta1004WebApplication1Context : DbContext
+    {
+        public RebecaBarrezueta1004WebApplication1Context (DbContextOptions<RebecaBarrezueta1004WebApplication1Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<RebecaBarrezueta1004WebApplication1.Models.Burguer> Burguer { get; set; } = default!;
+    }
+}
---
+++ Models/Burguer.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Runtime.CompilerServices;
+
+namespace RebecaBarrezueta1004WebApplication1.Models
+{
+    public class Burguer
+    {
+        public int BurguerId { get; set; }
+        [Required]
+        public string Name { get; set; }
+        public bool WithCheese { get; set; }
+
+        [Range(0.01, 9999.99)]
+        public decimal Precio { get; set; }
+    }
+}
